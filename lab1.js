d536245f672c29ef70bedfaf58f780ec94c34de5
Ayoosh <>
2025-05-07 11:28:42 +0300

    first commit

--- lab1.js
+++ lab1.js
@@ -1,4 +1,20 @@
 let h1 = document.getElementsByClassName("welcome")[0]
 h1.innerText = "hello world"
 h1.style.color = "red"
+h1.style.background = "yellow"
 console.log(h1);
+
+let p = document.getElementById("p1");
+p.style.fontWeight = "bold";
+p.style.color = "white"
+p.style.background = "gray"
+
+let img = document.getElementsByTagName("img")[0]
+img.src = "./logo-h.png";
+
+
+let items = document.querySelectorAll('.list');
+let texts = ["first", "second", "third"];
+for (let i = 0; i < items.length; i++) {
+  items[i].innerText = texts[i];
+}
